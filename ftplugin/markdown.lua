541debd73e29decc608c44a32d377e3ef4b9f9bd
Austin <austin.swanlaw@gmail.com>
2023-09-27 09:30:13 -0500

    Combine book and markdown filetypes
    Was having issues with bullets-vim not working with other plugins and I
    needed to combine it to only work in the markdown and book filetypes. It
    was easier to combine the two filetypes since they are the same and put
    the remaps there.

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,3 +1,25 @@
 local bufnr = vim.api.nvim_get_current_buf()
 vim.bo[bufnr].textwidth = 65
 vim.bo[bufnr].formatoptions = vim.bo.formatoptions .. 'a'
+
+function bullet_vim_maps()
+    local opts_1 = { buffer = bufnr, silent = true}
+    local opts_2 = { buffer = bufnr, silent = true, noremap = true}
+
+    vim.keymap.set('i', '<cr>', '<Plug>(bullets-newline)', opts_1)
+    vim.keymap.set('i', '<C-cr>', '<Plug>(bullets-newline)', opts_2)
+    vim.keymap.set('n', 'o', '<Plug>(bullets-newline)', opts_1)
+    vim.keymap.set({'v', 'n'}, 'gN', '<Plug>(bullets-renumber)', opts_1)
+    vim.keymap.set('n', '<Leader>x', '<Plug>(bullets-toggle-checkbox)', opts_1)
+    vim.keymap.set('i', '<Tab>', '<Plug>(bullets-demote)', opts_1)
+    vim.keymap.set('n', '>>', '<Plug>(bullets-demote)', opts_1)
+    vim.keymap.set('v', '>', '<Plug>(bullets-demote)', opts_1)
+    vim.keymap.set('i', '<S-Tab>', '<Plug>(bullets-promote)', opts_1)
+    vim.keymap.set('n', '<<', '<Plug>(bullets-promote)', opts_1)
+    vim.keymap.set('v', '<', '<Plug>(bullets-promote)', opts_1)
+end
+
+bullet_vim_maps()
+
+vim.cmd("echo 'Markdown Filetype Detected.'")
+
