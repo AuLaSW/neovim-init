132b91cc16081b5abf416da445f369cd86c1a870
Austin <austin.swanlaw@gmail.com>
2023-10-05 11:22:26 -0500

    Update colorschemes
    Moved the highlights to the colorscheme plugin file and removed plugins
    we have moved over to lazy.nvim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,25 @@
+local function highlights(mod)
+    vim.cmd([[
+        hi DiagnosticUnderlineInfo guifg=#7fa2ac cterm=underline gui=underline
+    ]])
+
+    local mod = require("gruvbox-baby.colors").config()
+   -- local colors = require("gruvbox-baby.colors").config()
+    vim.api.nvim_set_hl(0, "@chapter.begin", {fg = "#458588"})
+    vim.api.nvim_set_hl(0, "@chapter.end", {fg = "#b16286"})
+    vim.api.nvim_set_hl(0, "@import.id", {fg = mod.pink})
+    vim.api.nvim_set_hl(0, "@import.alias.old", {fg = mod.pink})
+    vim.api.nvim_set_hl(0, "@import.alias.new", {fg = mod.dark_gray})
+    vim.api.nvim_set_hl(0, "@variable.python", {fg = mod.dark_gray})
+    vim.api.nvim_set_hl(0, "@arguments.id", {fg = mod.magenta})
+    vim.api.nvim_set_hl(0, "@parameters.id", {fg = mod.pink})
+    vim.api.nvim_set_hl(0, "@parameters.type", {fg = mod.forest_green})
+    vim.api.nvim_set_hl(0, "@punctuation.delimiter.gitignore", {fg = mod.forest_green})
+    vim.api.nvim_set_hl(0, "@operator.gitignore", {fg = mod.orange})
+    vim.api.nvim_set_hl(0, "@annotate", {bg = mod.orange})
+end
+
+
 return {
     'luisiacc/gruvbox-baby',
     branch = 'main',
@@ -5,5 +27,8 @@ return {
     priority = 1000,
     config = function(_, opts)
         vim.cmd[[colorscheme gruvbox-baby]]
+
+        local mod = require("gruvbox-baby.colors").config()
+        highlights(mod)
     end,
 }
