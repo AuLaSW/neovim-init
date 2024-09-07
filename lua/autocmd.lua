845ef0ad4e3627395649ce9e18173681788a6ae2
Austin Swanlaw <austinswanlaw@Camerons-MacBook-Pro.local>
2024-09-06 21:15:36 -0500

    Update autocommands

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,6 +1,4 @@
-local api = vim.api
-
-api.nvim_create_autocmd(
+vim.api.nvim_create_autocmd(
     {'BufEnter'},
     {
         pattern = { '*.md', '*.book' },
@@ -10,7 +8,7 @@ api.nvim_create_autocmd(
     }
 )
 
-api.nvim_create_autocmd(
+vim.api.nvim_create_autocmd(
     {'BufLeave'},
     {
         pattern = { '*.md', '*.book' },
@@ -19,3 +17,18 @@ api.nvim_create_autocmd(
         end,
     }
 )
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+    callback = function (args)
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = args.buf,
+            callback = function ()
+                vim.lsp.buf.format({
+                    async = false,
+                    id = args.data.client_id
+                })
+            end
+        })
+    end
+})
